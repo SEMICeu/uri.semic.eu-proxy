892407e7e42cd845f44ae2ba7b38d0f00b9960cf
root <root@ip-172-31-25-69.eu-west-1.compute.internal>
2026-04-08 12:08:45 +0200

    align nginx config

--- urisemic.nginx.conf.d
+++ urisemic.nginx.conf.d
@@ -117,10 +117,51 @@ server {
     resolver 127.0.0.11 valid=10s;
     resolver_timeout 5s; 
 
+
+    location ~ /matomo(/?.*) {
+    add_header 'Access-Control-Allow-Origin' '*';
+    add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept';
+
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Uri /matomo;
+
+    set $match $1;
+    proxy_pass   http://matomo:80$match$is_args$args;
+    }
+
+    location ~ /virtuoso/(.*) {
+    add_header 'Access-Control-Allow-Origin' '*';
+    add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept';
+
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Uri /virtuoso;
+
+    set $match $1;
+    proxy_pass   http://exp-virtuoso:8890/$match$is_args$args;
+    }
+
+    location ~ /conductor(/?.*) {
+    add_header 'Access-Control-Allow-Origin' '*';
+    add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept';
+
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Uri /virtuoso;
+
+    set $match $1;
+    proxy_pass   http://exp-virtuoso:8890/conductor$match$is_args$args;
+    }
+
+
     location / {
-#proxy_set_header Host health.semic.eu:81 ;
+       #proxy_set_header Host health.semic.eu:81 ;
        proxy_set_header   Host $host;
-	proxy_pass http://virtuoso:81 ;
+	proxy_pass http://virtuoso:80 ;
             proxy_set_header   X-Real-IP $remote_addr;
             proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header   X-Forwarded-Proto $scheme;
@@ -1065,6 +1106,246 @@ it6              end
      } 
     proxy_pass  https://semiceu.github.io ;
     } 
+
+    location /2qy {
+
+    add_header 'Access-Control-Allow-Origin' '*';
+    add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept';
+
+    rewrite_by_lua_block {
+	 local urisemic = require("urisemic")
+	 local htmlmap = require("htmlmap")
+         
+	 local headers = ngx.req.get_headers(0) 
+-- no additional language awareness 
+--	 local langg = urisemic.order_accept_language() 
+--	 local langg2 = urisemic.return_mimetype('text/html', 'text/html', headers.accept) 
+
+-- if the uri has trailing slash
+
+	 local uri = ngx.var.uri
+	 local uri_notrailingslash = ngx.var.uri
+	 local uri_notrailingslashtest, err =  ngx.re.match(uri, ".*/$", "")
+
+	 if uri == "/2qy" then
+	       return ngx.redirect(uri .. "/2qy")
+         end
+	 if uri == "/2qy/" then
+	       return ngx.redirect(uri .. "2qy")
+         end
+
+         -- remove trailing slash and extension
+	 -- start
+	 if uri_notrailingslashtest then
+             uri_notrailingslash = ngx.re.sub(uri, "(.*)/", "$1")
+
+	 else
+	     if err then
+		 ngx.log(ngx.ERR, "error: ", err)
+	     end
+
+	 end
+
+         local entity_withextension,err2 = ngx.re.match(uri_notrailingslash,".*\\..*", "")
+	 local entity_noextension = uri_notrailingslash
+	 local entity_extension 
+
+	 if entity_withextension then
+             entity_noextension = ngx.re.sub(uri_notrailingslash, "(.*)\\..*", "$1")
+             entity_extension = ngx.re.sub(uri_notrailingslash, ".*\\.(.*)", "$1")
+
+	 else
+	     if err2 then
+		 ngx.log(ngx.ERR, "error: ", err2)
+	     end
+
+	 end
+	 -- end trailing slash and extension 
+
+	 local machineprocessable = true 
+         ngx.log(ngx.WARN, entity_noextension)
+         ngx.log(ngx.WARN, entity_extension)
+
+         if urisemic.ext(headers.accept) then
+	    -- an explicit accept header is given and it is a machine readible format 
+            -- check if there is an extension and if this is corresponds to the accept header 
+	    -- TODO check if the extension is machine readible format
+
+            local reqext = urisemic.ext(headers.accept)
+            local derived_extension
+	    if  entity_extension ~= nil then 
+	        if urisemic.supportedext(entity_extension) then 
+			if entity_extension ~= reqext then
+			    -- request extension has priority over the header choice
+			    derived_extension = entity_extension
+			else 
+			    derived_extension = reqext
+			end
+		else 
+			-- in case extension not supported then return according to the accept header
+			derived_extension = reqext
+	 	end	
+	    else 
+		derived_extension = reqext
+	    end
+        
+             uri = ngx.re.sub(entity_noextension, "^/2qy/(.*)", "/uri.semic.eu-puris/releases/2qy/$1." .. derived_extension, "o")
+         else
+            -- check if there is a machine readible file extension
+
+	     if entity_extension ~= nil and urisemic.supportedext(entity_extension) then
+                uri = ngx.re.sub(entity_noextension, "^/2qy/(.*)", "/uri.semic.eu-puris/releases/2qy/$1." .. entity_extension, "o")
+             else 
+		 
+	 	     machineprocessable = false
+
+		     if htmlmap.htmlmap[entity_noextension] then
+			    uri = htmlmap.htmlmap[entity_noextension]
+		     else
+			    -- point to a default html page on the r5r vocabulary
+--			    uri = ngx.re.sub(entity_noextension, "^/2qy/(.*)", "https://semiceu.github.io/RRMV/releases/0.1.1/", "o")
+			    uri = "https://semiceu.github.io/RRMV/releases/0.2.0/"
+		    end
+              end
+         end
+
+               ngx.log(ngx.WARN, machineprocessable)
+
+	 if machineprocessable then
+               ngx.log(ngx.WARN, uri)
+               ngx.req.set_uri(uri)
+	 else
+	-- in case of html pages trigger redirect immediately
+	       ngx.log(ngx.WARN, uri)
+	       return ngx.redirect(uri)
+	 end
+
+     } 
+    proxy_pass  https://semiceu.github.io ;
+    } 
+		
+    location /s1n {
+
+    add_header 'Access-Control-Allow-Origin' '*';
+    add_header 'Access-Control-Allow-Headers' 'Origin, X-Requested-With, Content-Type, Accept';
+
+    rewrite_by_lua_block {
+	 local urisemic = require("urisemic")
+	 local htmlmap = require("htmlmap")
+         
+	 local headers = ngx.req.get_headers(0) 
+-- no additional language awareness 
+--	 local langg = urisemic.order_accept_language() 
+--	 local langg2 = urisemic.return_mimetype('text/html', 'text/html', headers.accept) 
+
+-- if the uri has trailing slash
+
+	 local uri = ngx.var.uri
+	 local uri_notrailingslash = ngx.var.uri
+	 local uri_notrailingslashtest, err =  ngx.re.match(uri, ".*/$", "")
+
+	 if uri == "/s1n" then
+	       return ngx.redirect(uri .. "/s1n")
+         end
+	 if uri == "/s1n/" then
+	       return ngx.redirect(uri .. "s1n")
+         end
+
+
+
+
+         -- remove trailing slash and extension
+	 -- start
+	 if uri_notrailingslashtest then
+             uri_notrailingslash = ngx.re.sub(uri, "(.*)/", "$1")
+
+	 else
+	     if err then
+		 ngx.log(ngx.ERR, "error: ", err)
+	     end
+
+	 end
+
+         local entity_withextension,err2 = ngx.re.match(uri_notrailingslash,".*\\..*", "")
+	 local entity_noextension = uri_notrailingslash
+	 local entity_extension 
+
+	 if entity_withextension then
+             entity_noextension = ngx.re.sub(uri_notrailingslash, "(.*)\\..*", "$1")
+             entity_extension = ngx.re.sub(uri_notrailingslash, ".*\\.(.*)", "$1")
+
+	 else
+	     if err2 then
+		 ngx.log(ngx.ERR, "error: ", err2)
+	     end
+
+	 end
+	 -- end trailing slash and extension 
+
+	 local machineprocessable = true 
+         ngx.log(ngx.WARN, entity_noextension)
+         ngx.log(ngx.WARN, entity_extension)
+
+         if urisemic.ext(headers.accept) then
+	    -- an explicit accept header is given and it is a machine readible format 
+            -- check if there is an extension and if this is corresponds to the accept header 
+	    -- TODO check if the extension is machine readible format
+
+            local reqext = urisemic.ext(headers.accept)
+            local derived_extension
+	    if  entity_extension ~= nil then 
+	        if urisemic.supportedext(entity_extension) then 
+			if entity_extension ~= reqext then
+			    -- request extension has priority over the header choice
+			    derived_extension = entity_extension
+			else 
+			    derived_extension = reqext
+			end
+		else 
+			-- in case extension not supported then return according to the accept header
+			derived_extension = reqext
+	 	end	
+	    else 
+		derived_extension = reqext
+	    end
+        
+             uri = ngx.re.sub(entity_noextension, "^/s1n/(.*)", "/uri.semic.eu-puris/releases/s1n/$1." .. derived_extension, "o")
+         else
+            -- check if there is a machine readible file extension
+
+	     if entity_extension ~= nil and urisemic.supportedext(entity_extension) then
+                uri = ngx.re.sub(entity_noextension, "^/s1n/(.*)", "/uri.semic.eu-puris/releases/s1n/$1." .. entity_extension, "o")
+             else 
+		 
+	 	     machineprocessable = false
+
+		     if htmlmap.htmlmap[entity_noextension] then
+			    uri = htmlmap.htmlmap[entity_noextension]
+		     else
+			    -- point to a default html page on the core vocs
+--			    uri = ngx.re.sub(entity_noextension, "^/s1n/(.*)", "https://joinup.ec.europa.eu/solution/e-government-core-vocabularies", "o")
+--			    uri = "https://joinup.ec.europa.eu/solution/e-government-core-vocabularies"
+			    uri = "https://SEMICeu.github.io/uri.semic.eu-puris/releases/s1n/s1n.ttl"
+		    end
+              end
+         end
+
+               ngx.log(ngx.WARN, machineprocessable)
+
+	 if machineprocessable then
+               ngx.log(ngx.WARN, uri)
+               ngx.req.set_uri(uri)
+	 else
+	-- in case of html pages trigger redirect immediately
+	       ngx.log(ngx.WARN, uri)
+	       return ngx.redirect(uri)
+	 end
+
+     } 
+    proxy_pass  https://semiceu.github.io ;
+
+    }
+
 		
 
     location / {
